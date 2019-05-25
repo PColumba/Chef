201839f23de063d97adb7346179cfaf553ec46be
PColumba <pawel.agolab@gmail.com>
2019-05-25 15:43:03 +0200

    Chef #4 added favorite recipe button. closes #4

--- app/src/main/java/com/example/chef/search_result_detailed/RecipeDetailsActivity.java
+++ app/src/main/java/com/example/chef/search_result_detailed/RecipeDetailsActivity.java
@@ -1,12 +1,8 @@
 package com.example.chef.search_result_detailed;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
